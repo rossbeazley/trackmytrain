6410cf83027a97052c5ef61a745cb4cfd78b21d9
rossbeazley <ross@rossbeazley.co.uk>
2015-02-11 13:03:53 +0000

    First acceptance tests writing

---
+++ core/src/test/java/uk/co/rossbeazley/trackmytrain/android/TrackMyTrainTest.java
@@ -0,0 +1,21 @@
+package uk.co.rossbeazley.trackmytrain.android;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TrackMyTrainTest {
+
+    @Test @Ignore("WIP")
+    public void theOneWhereWeRequestDetailsOfAServiceAndTheResultsAreAnnounced() {
+
+    }
+
+    @Test @Ignore("WIP")
+    public void theOneWhereDetailsOfAServiceAreAlreadyLoaded_observingAnnouncesThatResult() {
+
+    }
+
+
+}
