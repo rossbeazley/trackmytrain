8cb2b8f0d67b9c1c2d23c39b7ad9616d749c6082
rossbeazley <ross@rossbeazley.co.uk>
2015-02-17 13:16:03 +0000

    removed local.properties from git

---
+++ core/src/test/java/uk/co/rossbeazley/trackmytrain/android/ServiceTest.java
@@ -0,0 +1,15 @@
+package uk.co.rossbeazley.trackmytrain.android;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * Created by beazlr02 on 17/02/2015.
+ */
+public class ServiceTest {
+
+    @Test @Ignore("wip")
+    public void theOneWhereWeSelectAServiceAndTrackingStarts() {
+
+    }
+}
