abfdb3803577fb6f9462cdbc37c17aea3afd82cb
rossbeazley <ross@rossbeazley.co.uk>
2015-02-24 22:01:45 +0000

    trying to write a test for the departures list

--- mobile/src/test/java/uk/co/rossbeazley/trackmytrain/android/mobile/DeparturesTest.java
+++ mobile/src/test/java/uk/co/rossbeazley/trackmytrain/android/mobile/DeparturesTest.java
@@ -1,8 +1,12 @@
 package uk.co.rossbeazley.trackmytrain.android.mobile;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
@@ -10,9 +14,14 @@ import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 import org.robolectric.util.ActivityController;
 
+import java.util.Arrays;
+import java.util.List;
+
 import uk.co.rossbeazley.trackmytrain.android.R;
+import uk.co.rossbeazley.trackmytrain.android.Train;
 import uk.co.rossbeazley.trackmytrain.android.mobile.Departures;
 
+import static org.hamcrest.CoreMatchers.hasItems;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
@@ -44,6 +53,23 @@ public class DeparturesTest {
         TextView viewById = (TextView) act.findViewById(R.id.to);
         String toText = String.valueOf(viewById.getText());
         assertThat(toText,is("CRL"));
+    }
+
+    @Test @Ignore("WIP")
+    public void displaysTheDepartureResultsForAJourney() {
+        final Train train = new Train("1", "", "", "");
+        final Train train1 = new Train("2", "", "", "");
+        List<Train> expected = Arrays.asList(train, train1);
+
+        act.findViewById(R.id.getdepartures).performClick();
+
+        final ListView departureList = (ListView) act.findViewById(R.id.departurelist);
+
+        //List<Train> trains = departureList.getAdapter().;
+
+
+
+        //assertThat(trains,hasItems(train,train1));
     }
 
 }
