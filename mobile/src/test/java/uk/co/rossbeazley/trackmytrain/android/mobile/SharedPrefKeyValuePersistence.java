cbf5ed8107ad1bd763ff4ceece57d73ea041223c
rossbeazley <ross@rossbeazley.co.uk>
2015-03-06 15:53:17 +0000

    starting to spec out the persistence test

---
+++ mobile/src/test/java/uk/co/rossbeazley/trackmytrain/android/mobile/SharedPrefKeyValuePersistence.java
@@ -0,0 +1,15 @@
+package uk.co.rossbeazley.trackmytrain.android.mobile;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * Created by beazlr02 on 06/03/2015.
+ */
+public class SharedPrefKeyValuePersistence {
+
+    @Test @Ignore("wip")
+    public void persistsAndRetreivesData() {
+
+    }
+}
