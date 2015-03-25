c3cceb83dc84177fc8fcffff79e4faff20026bbe
rossbeazley <ross@rossbeazley.co.uk>
2015-03-25 13:09:09 +0000

    extracting testing activtiy to be re-used

---
+++ mobile/src/test/java/uk/co/rossbeazley/trackmytrain/android/mobile/ActivityToHouseTesting.java
@@ -0,0 +1,10 @@
+package uk.co.rossbeazley.trackmytrain.android.mobile;
+
+import android.app.Activity;
+
+/**
+* Created by beazlr02 on 25/03/2015.
+*/
+public class ActivityToHouseTesting extends Activity {
+
+}
--- mobile/src/test/java/uk/co/rossbeazley/trackmytrain/android/mobile/RemembersDepartureQueryTest.java
+++ mobile/src/test/java/uk/co/rossbeazley/trackmytrain/android/mobile/RemembersDepartureQueryTest.java
@@ -1,6 +1,5 @@
 package uk.co.rossbeazley.trackmytrain.android.mobile;
 
-import android.app.Activity;
 import android.widget.TextView;
 
 import org.junit.Ignore;
@@ -23,7 +22,7 @@ public class RemembersDepartureQueryTest {
 
     @Test @Ignore("WIP, WTF, plan to migrate other DepartureQueryTest into this test where we poke view directly")
     public void theScreenLoadsWithLastQueriedFromStation() {
-        Testing activity = activity();
+        ActivityToHouseTesting activity = activity();
         activity.setContentView(R.layout.servicedetails);
 
         TextView viewById = (TextView) activity.findViewById(R.id.at);
@@ -35,17 +34,13 @@ public class RemembersDepartureQueryTest {
     }
 
 
-    private Testing activity() {
-        ActivityController<Testing> actCtl = Robolectric.buildActivity(Testing.class);
+    private ActivityToHouseTesting activity() {
+        ActivityController<ActivityToHouseTesting> actCtl = Robolectric.buildActivity(ActivityToHouseTesting.class);
         return actCtl.create()
                 .start()
                 .resume()
                 .visible()
                 .get();
-    }
-
-    public static class Testing extends Activity {
-
     }
 
 }
