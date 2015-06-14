44da7621ef8b921ffa06fcd293954a962ea54c52
rossbeazley <ross@rossbeazley.co.uk>
2015-06-14 21:42:30 +0100

    started next wearable test

---
+++ wear/src/test/java/uk/co/rossbeazley/trackmytrain/android/wear/WearableListenerServiceWill.java
@@ -0,0 +1,18 @@
+package uk.co.rossbeazley.trackmytrain.android.wear;
+
+import org.junit.Test;
+
+public class WearableListenerServiceWill {
+
+    @Test public void
+    registerHostNodeIDWhenHostBroadcastsSelf() {
+
+    }
+
+    @Test public void
+    announceTrackingStartedWhenBroadcastReceived() {
+
+    }
+
+
+}
