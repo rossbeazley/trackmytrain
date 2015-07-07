6fc8d3ca1268a56ebf35ed7c2cd5bdb2f3c57896
rossbeazley <ross@rossbeazley.co.uk>
2015-07-07 22:17:50 +0100

    rename test class to make it less about starting tracking and more about just tracking

--- mobile/src/test/java/uk/co/rossbeazley/trackmytrain/android/mobile/TrackingOnWearable.java
+++ mobile/src/test/java/uk/co/rossbeazley/trackmytrain/android/mobile/TrackingOnWearable.java
@@ -10,7 +10,7 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-public class TrackingStartedOnWearable {
+public class TrackingOnWearable {
 
     @Test
     public void
