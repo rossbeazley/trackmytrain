59d2d97c1443ca24d9b1e142d35c7561d4415720
rossbeazley <ross@rossbeazley.co.uk>
2015-07-20 22:48:43 +0100

    started adding tracking view and nasty animation on results list

--- mobile/src/main/java/uk/co/rossbeazley/trackmytrain/android/mobile/departures/DeparturesScreen.java
+++ mobile/src/main/java/uk/co/rossbeazley/trackmytrain/android/mobile/departures/DeparturesScreen.java
@@ -3,15 +3,18 @@ package uk.co.rossbeazley.trackmytrain.android.mobile.departures;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.TextView;
 
 import uk.co.rossbeazley.trackmytrain.android.R;
 import uk.co.rossbeazley.trackmytrain.android.TMTError;
+import uk.co.rossbeazley.trackmytrain.android.TrainViewModel;
 import uk.co.rossbeazley.trackmytrain.android.departures.presentation.DeparturesQueryView;
 import uk.co.rossbeazley.trackmytrain.android.departures.presentation.DeparturesQueryViewModel;
 import uk.co.rossbeazley.trackmytrain.android.departures.presentation.DeparturesView;
 import uk.co.rossbeazley.trackmytrain.android.departures.presentation.DeparturesViewModel;
 import uk.co.rossbeazley.trackmytrain.android.mobile.FindsView;
 import uk.co.rossbeazley.trackmytrain.android.mobile.TrackMyTrainApp;
+import uk.co.rossbeazley.trackmytrain.android.trackedService.ServiceView;
 
 public class DeparturesScreen {
 
@@ -43,6 +46,46 @@ public class DeparturesScreen {
 
             }
 
+        });
+        TrackMyTrainApp.instance.attach(new ServiceView() {
+
+            View container = findsView.findViewById(R.id.trackedService);
+            TextView scheduledTime = (TextView) container.findViewById(R.id.scheduledtime);
+            TextView expectedTime = (TextView) container.findViewById(R.id.estimatedtime);
+            TextView platorm = (TextView) container.findViewById(R.id.platform);
+
+            @Override
+            public void present(final TrainViewModel train) {
+                container.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        scheduledTime.setText(train.scheduledTime());
+                        expectedTime.setText(train.estimatedTime());
+                        platorm.setText(train.platform());
+                    }
+                });
+            }
+
+            @Override
+            public void hide() {
+                container.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        container.setVisibility(View.GONE);
+                    }
+                });
+            }
+
+            @Override
+            public void trackingStarted() {
+                container.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        container.setVisibility(View.VISIBLE);
+                    }
+                });
+
+            }
         });
     }
 
