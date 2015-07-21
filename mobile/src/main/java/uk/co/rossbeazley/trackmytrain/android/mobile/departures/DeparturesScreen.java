34f91a235d5ffaec3f47ca5a6093e85bfa9e4857
rossbeazley <ross@rossbeazley.co.uk>
2015-07-21 21:51:43 +0100

    extract annon classes to named classes, the NRE logo view and the Tracked Service View

--- mobile/src/main/java/uk/co/rossbeazley/trackmytrain/android/mobile/departures/DeparturesScreen.java
+++ mobile/src/main/java/uk/co/rossbeazley/trackmytrain/android/mobile/departures/DeparturesScreen.java
@@ -27,77 +27,8 @@ public class DeparturesScreen {
         departuresQueryView = new AndroidDeparturesQueryView(findsView, inputMethodManager);
         TrackMyTrainApp.instance.attach(departureView);
         TrackMyTrainApp.instance.attach(departuresQueryView);
-        TrackMyTrainApp.instance.attach(new DeparturesView() {
-
-            private View nreLogo = findsView.findViewById(R.id.powered_by_nre);
-
-            @Override
-            public void present(DeparturesViewModel trains) {
-
-            }
-
-            @Override
-            public void loading() {
-                final ViewGroup viewById = (ViewGroup) (findsView.findViewById(R.id.servicedetails));
-                viewById.removeView(nreLogo);
-                viewById.removeView(findsView.findViewById(R.id.powered_by_nre_spacer));
-                TrackMyTrainApp.instance.detach(this);
-            }
-
-            @Override
-            public void error(TMTError error) {
-
-            }
-
-        });
-        TrackMyTrainApp.instance.attach(new ServiceView() {
-
-            View container = findsView.findViewById(R.id.trackedService);
-            TextView scheduledTime = (TextView) container.findViewById(R.id.scheduledtime);
-            TextView expectedTime = (TextView) container.findViewById(R.id.estimatedtime);
-            TextView platorm = (TextView) container.findViewById(R.id.platform);
-            Button stopTracking = (Button) container.findViewById(R.id.trackbutton);
-
-            @Override
-            public void present(final TrainViewModel train) {
-                container.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        container.setVisibility(View.VISIBLE);
-                        scheduledTime.setText(train.scheduledTime());
-                        expectedTime.setText(train.estimatedTime());
-                        platorm.setText(train.platform());
-                        stopTracking.setOnClickListener(new View.OnClickListener() {
-                            @Override
-                            public void onClick(View view) {
-                                TrackMyTrainApp.instance.unwatch();
-                            }
-                        });
-                    }
-                });
-            }
-
-            @Override
-            public void hide() {
-                container.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        container.setVisibility(View.GONE);
-                    }
-                });
-            }
-
-            @Override
-            public void trackingStarted() {
-                container.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        container.setVisibility(View.VISIBLE);
-                    }
-                });
-
-            }
-        });
+        TrackMyTrainApp.instance.attach(new NRELogoView(findsView));
+        TrackMyTrainApp.instance.attach(new TrackedServiceView(findsView));
     }
 
     public void tearDown() {
@@ -105,5 +36,94 @@ public class DeparturesScreen {
         TrackMyTrainApp.instance.detach(departuresQueryView);
         departureView=null;
         departuresQueryView=null;
+    }
+
+    private static class TrackedServiceView implements ServiceView {
+
+        private final FindsView findsView;
+        View container;
+        TextView scheduledTime;
+        TextView expectedTime;
+        TextView platorm;
+        Button stopTracking;
+
+        public TrackedServiceView(FindsView findsView) {
+            this.findsView = findsView;
+            container = findsView.findViewById(R.id.trackedService);
+            scheduledTime = (TextView) container.findViewById(R.id.scheduledtime);
+            expectedTime = (TextView) container.findViewById(R.id.estimatedtime);
+            platorm = (TextView) container.findViewById(R.id.platform);
+            stopTracking = (Button) container.findViewById(R.id.trackbutton);
+        }
+
+        @Override
+        public void present(final TrainViewModel train) {
+            container.post(new Runnable() {
+                @Override
+                public void run() {
+                    container.setVisibility(View.VISIBLE);
+                    scheduledTime.setText(train.scheduledTime());
+                    expectedTime.setText(train.estimatedTime());
+                    platorm.setText(train.platform());
+                    stopTracking.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            TrackMyTrainApp.instance.unwatch();
+                        }
+                    });
+                }
+            });
+        }
+
+        @Override
+        public void hide() {
+            container.post(new Runnable() {
+                @Override
+                public void run() {
+                    container.setVisibility(View.GONE);
+                }
+            });
+        }
+
+        @Override
+        public void trackingStarted() {
+            container.post(new Runnable() {
+                @Override
+                public void run() {
+                    container.setVisibility(View.VISIBLE);
+                }
+            });
+
+        }
+    }
+
+    private static class NRELogoView implements DeparturesView {
+
+        private final FindsView findsView;
+        private View nreLogo;
+
+        public NRELogoView(FindsView findsView) {
+            this.findsView = findsView;
+            nreLogo = findsView.findViewById(R.id.powered_by_nre);
+        }
+
+        @Override
+        public void present(DeparturesViewModel trains) {
+
+        }
+
+        @Override
+        public void loading() {
+            final ViewGroup viewById = (ViewGroup) (findsView.findViewById(R.id.servicedetails));
+            viewById.removeView(nreLogo);
+            viewById.removeView(findsView.findViewById(R.id.powered_by_nre_spacer));
+            TrackMyTrainApp.instance.detach(this);
+        }
+
+        @Override
+        public void error(TMTError error) {
+
+        }
+
     }
 }
