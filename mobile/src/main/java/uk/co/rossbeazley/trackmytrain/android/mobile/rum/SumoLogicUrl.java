2329f90b073c8182d1cf88ef212dfc699a215221
rossbeazley <ross@rossbeazley.co.uk>
2015-08-17 12:55:48 +0100

    created a sumo logic endpoint to send rum data to

---
+++ mobile/src/main/java/uk/co/rossbeazley/trackmytrain/android/mobile/rum/SumoLogicUrl.java
@@ -0,0 +1,11 @@
+package uk.co.rossbeazley.trackmytrain.android.mobile.rum;
+
+/**
+ * Created by beazlr02 on 17/08/15.
+ */
+public class SumoLogicUrl {
+
+    public String toString() {
+        return "https://collectors.eu.sumologic.com/receiver/v1/http/ZaVnC4dhaV2_CNJQ3R1ERamcWQ_NRd25PVNlH2WX3kzTkTr0WUffF1-XGL21ouyBrEMgvm0Zdui_32Ow8zh3_X8HZIDG7Aw6_EjU165ZZjMgaKMdYLBmLQ==";
+    }
+}
