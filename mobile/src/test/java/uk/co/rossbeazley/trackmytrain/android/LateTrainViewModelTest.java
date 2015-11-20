90f7d45064785647fbefe85913f7fe8a299dd746
rossbeazley <ross@rossbeazley.co.uk>
2015-11-20 13:14:24 +0000

    renamed LateTrainTest to include the view model under test in its name

--- mobile/src/test/java/uk/co/rossbeazley/trackmytrain/android/LateTrainViewModelTest.java
+++ mobile/src/test/java/uk/co/rossbeazley/trackmytrain/android/LateTrainViewModelTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
-public class LateTrainTest {
+public class LateTrainViewModelTest {
 
     @Test
     public void aLateTrain() {
