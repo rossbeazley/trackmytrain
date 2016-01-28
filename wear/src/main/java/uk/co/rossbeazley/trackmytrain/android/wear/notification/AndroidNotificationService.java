aa323e4958bf2005fb9a8587253f5b64ef923008
rossbeazley <ross@rossbeazley.co.uk>
2016-01-28 21:51:26 +0000

    uses new cancel icon in wear notificatio n

--- wear/src/main/java/uk/co/rossbeazley/trackmytrain/android/wear/notification/AndroidNotificationService.java
+++ wear/src/main/java/uk/co/rossbeazley/trackmytrain/android/wear/notification/AndroidNotificationService.java
@@ -61,8 +61,10 @@ public class AndroidNotificationService extends Service implements WearNotificat
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        String action = intent.getAction();
-        notificationPresenter.serviceAction(action);
+        if(intent!=null) {
+            String action = intent.getAction();
+            notificationPresenter.serviceAction(action);
+        }
         return START_STICKY;
     }
 
@@ -71,7 +73,7 @@ public class AndroidNotificationService extends Service implements WearNotificat
 
     public void notify(String contentTitle, String contentText) {
         Notification.Action action;
-        action = new Notification.Action.Builder(R.drawable.ic_stop_tracking, "Stop Tracking", stopTrackingPendingIntent(this)).build();
+        action = new Notification.Action.Builder(R.drawable.ic_menu_close_clear_cancel, "Stop Tracking", stopTrackingPendingIntent(this)).build();
         Intent intent = new Intent(this, TrackingActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         final Notification.Builder builder = new Notification.Builder(this)
